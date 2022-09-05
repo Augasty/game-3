53bd4be6e532a5851d292c0b26fdb3bc43239316
Augasty <sayaksengupta140@gmail.com>
2022-09-05 20:03:56 +0530

    commiting ffs

---
+++ code/level.py
@@ -0,0 +1,85 @@
+import pygame
+from tiles import Tile
+from player import Player
+from settings import tile_size, screen_width
+class Level:
+    def __init__(self,level_data,surface):
+
+        #level setup
+        self.display_surface = surface
+        self.setup_level(level_data)
+
+        self.world_shift = 0
+    def setup_level(self,layout):
+        self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+        for row_index,row in enumerate(layout):
+            for col_index,cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+                if cell == 'X':
+                    tile = Tile((x,y),tile_size)
+                    self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x,y))
+                    self.player.add(player_sprite)
+
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+        if player_x < screen_width / 4 and direction_x<0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width * 0.75 and direction_x>0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
+    #COLLISION
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+        #HORIZONTAL
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+
+        #VERTICAL
+    def vertical_movement_collision(self):
+        player = self.player.sprite
+        player.apply_gravity()
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0
+                    player.on_ground = True
+                elif player.direction.y < 0:
+                    player.rect.top = sprite.rect.bottom
+                    player.direction.y = 0
+                    player.on_ceiling = True
+
+        if player.on_ground and player.direction.y < 0 or player.direction.y > 1:
+            player.on_ground = False
+
+        if player.on_ceiling and player.direction.y > 0:
+            player.on_ceiling = False
+
+    def run(self):
+        #level tiles
+        self.tiles.update(self.world_shift)
+        self.tiles.draw(self.display_surface)
+        self.scroll_x()
+        #player
+
+        self.player.update()
+        self.horizontal_movement_collision()
+        self.vertical_movement_collision()
+        self.player.draw(self.display_surface)
---
+++ code/player.py
@@ -0,0 +1,104 @@
+import pygame
+from support import import_folder
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        self.import_character_assets()
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        self.image = self.animations['idle'][self.frame_index]
+        # self.image = pygame.Surface((32,64))
+        # self.image.fill('red')
+        self.rect = self.image.get_rect(topleft = pos)
+
+        #MOVEMENT
+        self.direction = pygame.math.Vector2(0,0)
+        self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
+        self.status = 'idle'
+        self.facing_right = True
+        self.on_ground = False
+        self.on_ceiling = False
+        self.on_left = False
+        self.on_right = False
+
+
+
+
+
+    def import_character_assets(self):
+        character_path = './graphics/character/'
+        self.animations = {'idle':[],'run':[],'jump':[],'fall':[]}
+
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = 'jump'
+        elif self.direction.y > 1:
+            self.status = 'fall'
+        else:
+            if self.direction.x != 0:
+                self.status = 'run'
+            else:
+                self.status = 'idle'
+
+
+
+
+    def animate(self):
+        animation = self.animations[self.status]
+
+        #loop over the frame index
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+        image = animation[int(self.frame_index)]
+        if self.facing_right:
+            self.image = image
+        else:
+            # flipped_image = pygame.transform.flip(image,True,True)
+            flipped_image = pygame.transform.flip(image,True,False)
+            self.image = flipped_image
+
+        #set the rect
+        if self.on_ground:
+            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+        elif self.on_ceiling:
+            self.rect = self.image.get_rect(midtop = self.rect.midtop)
+        else:
+            self.rect = self.image.get_rect(midtop = self.rect.midtop)
+
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_d]:
+            self.direction.x = 1
+            self.facing_right = True
+        elif keys[pygame.K_a]:
+            self.direction.x = -1
+            self.facing_right = False
+        else:
+            self.direction.x = 0
+
+        if keys[pygame.K_w] and self.on_ground:
+            self.jump()
+        elif keys[pygame.K_s]:
+            self.direction.y += 1
+        # else:
+        #     self.direction.y = 0
+    def jump(self):
+        self.direction.y = self.jump_speed
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+    def update(self):
+
+        self.get_input()
+        self.get_status()
+        self.animate()
